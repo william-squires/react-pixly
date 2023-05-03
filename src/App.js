ae3827465fdbb93c553cc171e42264b7e84e4fef
William Squires <william.squiresii@gmail.com>
2023-05-03 12:59:27 -0500

    added axios. built UploadForm

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import UploadForm from './UploadForm';
 
 function App() {
   return (
     <div className="App">
-
+      <UploadForm />
     </div>
   );
 }
---
+++ src/UploadForm.js
@@ -0,0 +1,52 @@
+import { useState } from "react"
+import axios from "axios"
+
+
+/** form for uploading images to backend
+ * 
+ * Props
+ * -none
+ * 
+ * State
+ * -formData: {file to upload}
+ * 
+ * App ->UploadForm
+ */
+function UploadForm() {
+    const [formData, setFormData] = useState("")
+    console.log(formData);
+
+    /** Update local state w/curr state of input elem */
+    function handleChange(evt) {
+        const f = evt.target.files[0]
+        setFormData(f);
+    }
+
+    async function handleSubmit(evt) {
+        evt.preventDefault();
+        const fData = new FormData();
+        fData.append("file", formData);
+        await axios.post("http://localhost:5000/",
+            fData,
+            {
+                headers: {
+                    "Content-Type": "multipart/form-data",
+                    'Access-Control-Allow-Origin': '*',
+                }
+            });
+        setFormData("");
+    }
+    return (
+        <form onSubmit={handleSubmit}>
+            <input
+                type="file"
+                accept="image/*"
+                value={formData.file}
+                onChange={handleChange}>
+            </input>
+            <button>Upload!</button>
+        </form>
+    )
+}
+
+export default UploadForm;
