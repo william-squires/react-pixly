cd10112dfa200c68cbe2ce10ddd21873c5a3559a
William Squires <william.squiresii@gmail.com>
2023-05-05 12:21:09 -0500

    carousel now gets images from database

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,5 +1,7 @@
+import { useEffect, useState } from "react";
 import "./Homepage.css";
 import Carousel from "react-bootstrap/Carousel";
+import axios from "axios";
 
 /** Homepage
  *
@@ -11,40 +13,33 @@ import Carousel from "react-bootstrap/Carousel";
  */
 
 function Homepage() {
+  const [imageUrls, setImageUrls] = useState(null)
+
+  useEffect(function getCarouselImagesOnMount() {
+    async function getImages() {
+      const resp = await axios.get(
+        `http://localhost:5000/random?count=5`,
+        { headers: { 'Access-Control-Allow-Origin': '*' } });
+        setImageUrls(resp.data)
+    }
+    getImages()
+  }, []) 
+
+  if (!imageUrls) {
+    return <h1>Loading</h1>
+  }
+
   return (
     <div className="Homepage">
       <h1 className="Homepage welcome">Welcome to pixly</h1>
-      <Carousel>
-        <Carousel.Item interval={4000}>
+      <Carousel className="carousel">
+        {imageUrls.map((url, idx) => <Carousel.Item key={idx} interval={2000}>
           <img
-            className="d-block w-100"
-            src="holder.js/800x400?text=First slide&bg=373940"
-            alt="First slide"
+            className="carousel-img"
+            src={url.url}
+            alt={`Slide ${idx + 1}`}
           />
-          <Carousel.Caption>
-            <h3>First slide label</h3>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item interval={4000}>
-          <img
-            className="d-block w-100"
-            src="holder.js/800x400?text=Second slide&bg=282c34"
-            alt="Second slide"
-          />
-          <Carousel.Caption>
-            <h3>Second slide label</h3>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="holder.js/800x400?text=Third slide&bg=20232a"
-            alt="Third slide"
-          />
-          <Carousel.Caption>
-            <h3>Third slide label</h3>
-          </Carousel.Caption>
-        </Carousel.Item>
+        </Carousel.Item>)}
       </Carousel>
     </div>
   );
